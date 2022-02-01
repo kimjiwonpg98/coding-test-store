6d100956d35d86456f53eb01b7c800edadf8a81d
jiwonpg98 <jiwonpg98@gmail.com>
2022-02-01 23:09:01 +0900

    Add(programmers): level2추가

--- 프로그래머스/level2/배달.js
+++ 프로그래머스/level2/배달.js
@@ -1,60 +1,33 @@
 function solution(N, road, K) {
-  const visited = new Array(N).fill(0);
-  const minVisited = new Array(N).fill(0);
-  const roads = Array.from(Array(N), () => Array(N).fill(0));
-  const inf = 100000;
-  let answer = 1;
+  const roads = Array.from({ length: N + 1 }, () => []);
+  const route = Array.from({ length: N + 1 }, () => Infinity);
+  const rocate = [];
 
-  road = road.sort((a, b) => {
-    b[2] - a[2];
+  road.map((x) => {
+    roads[x[0]].push({
+      to: x[1],
+      cost: x[2],
+    });
+    roads[x[1]].push({
+      to: x[0],
+      cost: x[2],
+    });
   });
 
-  for (let i of road) {
-    roads[i[0] - 1][i[1] - 1] = i[2];
-  }
+  route[1] = 0;
+  rocate.push({ to: 1, cost: 0 });
 
-  for (let count in roads) {
-    for (let town in roads[count]) {
-      if (count !== town && roads[count][town] === 0) {
-        roads[count][town] = inf;
+  while (rocate.length !== 0) {
+    rocate.sort((a, b) => a.cost - b.cost);
+
+    const { to, cost } = rocate.pop();
+    roads[to].forEach((nextNode) => {
+      if (route[nextNode.to] > route[to] + nextNode.cost) {
+        route[nextNode.to] = route[to] + nextNode.cost;
+        rocate.push(nextNode);
       }
-    }
+    });
   }
 
-  function getSmallIndex() {
-    let min = inf;
-    let index = 0;
-    for (let j = 0; j < N; j++) {
-      if (minVisited[j] < min && !visited[j]) {
-        min = minVisited[j];
-        index = j;
-      }
-    }
-    return index;
-  }
-
-  function cal(start) {
-    for (let k = 0; k < N; k++) {
-      minVisited[k] = roads[start][k];
-    }
-    visited[start] = 1;
-    for (let m = 0; m < N - 2; m++) {
-      let current = getSmallIndex();
-      visited[current] = 1;
-      for (let n = 0; n < N; n++) {
-        if (!visited[n]) {
-          if (minVisited[current] + roads[current][n] < minVisited[n]) {
-            minVisited[n] = minVisited[current] + roads[current][n];
-          }
-        }
-      }
-    }
-  }
-
-  cal(0);
-  minVisited.forEach((x) => {
-    if (x <= K) answer += 1;
-  });
-
-  return answer;
+  return route.filter((x) => x <= K).length;
 }
