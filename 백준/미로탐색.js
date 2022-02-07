d95163c0bab702779272abb6a6d442f33b3d7136
jiwonpg98 <jiwonpg98@gmail.com>
2022-02-07 12:31:23 +0900

    Add(backjun): 미로탐색 추가

---
+++ 백준/미로탐색.js
@@ -0,0 +1,45 @@
+const readline = require("readline");
+
+let input = [];
+let maze = [];
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", (line) => input.push(line.trim())).on("close", () => {
+  const [n, m] = input[0].split(" ").map(Number);
+  input.splice(0, 1);
+  for (let i = 0; i < n; i++) {
+    maze.push(input[i].split("").map(Number));
+  }
+
+  const dx = [-1, 1, 0, 0];
+  const dy = [0, 0, -1, 1];
+  const queue = [];
+  const visit = Array.from(new Array(n), () => new Array(m).fill(false));
+
+  visit[0][0] = true;
+  queue.push([0, 0]);
+
+  function escape(queue, visit, n, m) {
+    while (queue.length) {
+      const v = queue.shift();
+      const [x, y] = v;
+      for (let i = 0; i < dx.length; i++) {
+        let nx = x + dx[i];
+        let ny = y + dy[i];
+
+        if (nx > -1 && nx < n && ny > -1 && ny < m) {
+          if (visit[nx][ny] === false && maze[nx][ny] !== 0) {
+            queue.push([nx, ny]);
+            maze[nx][ny] = maze[x][y] + 1;
+            visit[nx][ny] = true;
+          }
+        }
+      }
+    }
+  }
+  escape(queue, visit, n, m);
+  console.log(maze[n - 1][m - 1]);
+});
